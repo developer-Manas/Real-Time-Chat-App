775ec6ccfbe829bdc40525d8adb1aa023c2c6976
developer-Manas <Manaskumar9102@gmail.com>
2026-04-09 03:15:02 +0530

    add paths

---
+++ src/UserManager.ts
@@ -0,0 +1,30 @@
+interface User {
+    name: string;
+    id: string;
+}
+
+interface Room {
+    users: User[]
+}
+
+export class UserManager {
+    private users : Map<string, Room>; 
+    constructor() {
+        this.users = new Map<string, Room> 
+    }
+
+    addUser(name: string, userId: string, roomId: string, socket: WebSocket) {
+        if (!this.users.get(roomId)) {
+            this.users.set(roomId, {
+                users: []
+            })
+        }
+        this.users.get(roomId)?.users.push({
+            id:  userId,  
+            name
+        })
+    }
+    removeUser(roomId: string, userId: string) {
+
+    }
+}
